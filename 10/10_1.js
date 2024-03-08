cb426f63e518527e965868aead1041d6b73d1307
LinkDiscovery <shin920111@gmail.com>
2024-03-08 16:19:37 +0900

    0308

--- 10/10_1.js
+++ 10/10_1.js
@@ -7,20 +7,27 @@ document.addEventListener("DOMContentLoaded", () => {
     bt.addEventListener("click", () => {
         nums = []; // 배열 초기화 
         //nums.length = 0 > 배열 초기화 
+
         // 1~45까지 랜덤 수 생성 , 랜덤 수가 7개 만들어지고 중복되지 않게 
         while (nums.length < 7) {
             let n = Math.floor(Math.random() * 45) + 1
-            nums.push(n)
+            if(!nums.includes(n)) nums.push(n)
         }
 
+//방법 1
+        // let tags = '';
+        // nums.map((v, i)=>{
+        //     tags = tags + `<span class = "sp${parseInt(v/10)}">${v}</span>`;
+        //     if(i === 5)
+        //         tags = tags + `<span class= i>+</span>`
+//방법 2 
 
-        let tags = '';
-        nums.map((v)=>{
-            tags = tags + `<span class = "sp${parseInt(v/10)}">${v}</span>`;
-        })
-
-        numDisp.innerHTML = tags;
+        nums =  nums.map((v)=>
+        `<span class = "sp${parseInt(v/10)}">${v}</span>`)
+        nums.splice(6,0,`<span>+</span>`)
+        numDisp.innerHTML = nums.toString().replaceAll(',','') // 최종적으로 뿌리는 것 .
+        
     })
-
+   
 
 });
